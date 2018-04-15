52da3da3da6964ce2b6c18c9815f9acc474fd45c
shepheam <shepheam@rose-hulman.edu>
2018-04-15 17:55:42 -0400

    done

--- src/m3_captial_t_class.py
+++ src/m3_captial_t_class.py
@@ -246,7 +246,7 @@ class CapitalT(object):
           :type dy: int
         """
         # --------------------------------------------------------------
-        # TODO: 6.
+        # DONE: 6.
         #   READ the above specification, including the Example.
         #   Implement and test this method by uncommenting the appropriate
         #     run_test method in main. Compare the graphics window to
@@ -283,7 +283,7 @@ class CapitalT(object):
           :rtype: CapitalT
         """
         # --------------------------------------------------------------
-        # TODO: 7.
+        # DONE: 7.
         #   READ the above specification, including the Example.
         #   Implement and test this method by uncommenting the appropriate
         #     run_test method in main. Compare the graphics window to
